59aa4cd1ccfe06fc897b529f4d00ac5830d6ec07
CreadwordGOD <75349962+cdnflyint@users.noreply.github.com>
2021-12-18 15:44:44 +0800

    Update umi.js

--- @1.4.3User/user/umi.js
+++ @1.4.3User/user/umi.js
@@ -27969,9 +27969,9 @@
                     className: "text-dark"
                 }, window.settings.title || "Flyint")), i.a.createElement("div", {
                     className: "text-uppercase font-w700 font-size-sm text-muted"
-                }, "禁止临时邮箱注册,否则封禁!"), i.a.createElement("p", {
+                }, "禁止临时邮箱注册!"),i.a.createElement("div", {
                     className: "text-uppercase font-w700 font-size-sm text-muted"
-                }, window.settings.description || "Flyint")), s ? i.a.createElement("div", {
+                }, "禁止黑灰产人士!")), s ? i.a.createElement("div", {
                     className: "content content-full text-center"
                 }, i.a.createElement("div", {
                     className: "spinner-grow text-primary",
